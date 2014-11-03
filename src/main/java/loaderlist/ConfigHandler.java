2981340c28a4cffdd44bc5a5276bfdbfc2947abb
TehNut <tehnut@tehnut.com>
2014-11-02 21:16:43 -0800

    Add configs that I probably should have already had...

--- src/main/java/loaderlist/ConfigHandler.java
+++ src/main/java/loaderlist/ConfigHandler.java
@@ -8,16 +8,25 @@ public class ConfigHandler {
 
 	public static Configuration config;
 
+	public static String general = "general";
+
 	public static String logName;
+	public static boolean logChunkForce;
+	public static boolean logChunkUnforce;
+	public static boolean showLoggingInConsole;
 
 	public static void init(File file) {
 		config = new Configuration(file);
 		syncConfig();
+		config.load();
 	}
 
 	public static void syncConfig() {
 
-		logName = config.get("general", "logName", "ChunkLoaderList", "Name for the chunkloader list file. A .txt extension will automagically be applied.").getString();
+		logName = config.get(general, "logName", "ChunkLoaderList", "Name for the chunkloader list file. A .txt extension will automagically be applied.").getString();
+		logChunkForce = config.get(general, "logChunkForce", true, "Toggle logging of forcing chunks.").getBoolean(logChunkForce);
+		logChunkUnforce = config.get(general, "logChunkUnforce", false, "Toggle logging of unforcing chunks.").getBoolean(logChunkUnforce);
+		showLoggingInConsole = config.get(general, "showLoggingInConsole", false, "Show the logging in the console. Generally only used for debugging purposes.").getBoolean(showLoggingInConsole);
 
 		config.save();
 	}
--- src/main/java/loaderlist/EventHandler.java
+++ src/main/java/loaderlist/EventHandler.java
@@ -7,13 +7,21 @@ public class EventHandler {
 
 	@SubscribeEvent
 	public void onChunkForce(ForgeChunkManager.ForceChunkEvent event) {
-		Utils.writeCLLog(event.ticket.getModId() + " forcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId), ConfigHandler.logName + ".txt");
-		LoaderList.logger.info(event.ticket.getModId() + " forcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId));
+		if (ConfigHandler.logChunkForce) {
+			Utils.writeCLLog(event.ticket.getModId() + " forcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId), ConfigHandler.logName + ".txt");
+			if (ConfigHandler.showLoggingInConsole) {
+				LoaderList.logger.info(event.ticket.getModId() + " forcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId));
+			}
+		}
 	}
 
 	@SubscribeEvent
 	public void onChunkUnforce(ForgeChunkManager.UnforceChunkEvent event) {
-		Utils.writeCLLog(event.ticket.getModId() + " unforcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId), ConfigHandler.logName + ".txt");
-		LoaderList.logger.info(event.ticket.getModId() + " unforcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId));
+		if (ConfigHandler.logChunkUnforce) {
+			Utils.writeCLLog(event.ticket.getModId() + " unforcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId), ConfigHandler.logName + ".txt");
+			if (ConfigHandler.showLoggingInConsole) {
+				LoaderList.logger.info(event.ticket.getModId() + " unforcing the loading of the chunk at *X = " + String.valueOf(event.location.getCenterXPos()) + "* and *Z =" + String.valueOf(event.location.getCenterZPosition()) + "* in dimension " + String.valueOf(event.ticket.world.provider.dimensionId));
+			}
+		}
 	}
 }
